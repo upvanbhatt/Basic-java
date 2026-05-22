43ff23f5f9ac2b43ea75788731b591c63b757ca2
upvanbhatt <upvanbhatt@gmail.com>
2026-05-22 13:41:41 +0530

    assignment

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+class Assignment
+{
+    public static void main(String args[])
+    {
+        int a;
+        a=10; // Simple ,here = is assignment operator
+        System.out.println(a);
+        System.out.println("Assignment Coumpound operator");
+        a+=10; // (a=a+10)
+        System.out.println(a);
+        a-=10; // (a=a-10)
+        System.out.println(a);
+
+    }
+}
+    
+
